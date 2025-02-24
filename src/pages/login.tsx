2f35227ef011b78989e1f46c2fdd3544e16c8f6c
Ricardo Atakiama <ricardo_cssg@hotmail.com>
2025-02-24 03:06:58 -0300

    feat: Shadcn configurado com tailwind 3 e react sem framework

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React from 'react';
 
 export default function Login() {
   return (
-    <main><div className="p-6 bg-blue-100 grow">
-    <h1 className="text-3xl font-bold text-blue-800">
-      Hello, Tailwind CSS with React 17!
-    </h1>
-    <p className="mt-4 text-gray-700">
-      This is a simple setup of Tailwind CSS in a React v17 project.
-    </p>
-  </div></main>
-  )
+    <main>
+      <div className="p-6 bg-blue-100 grow">
+        <h1 className="text-3xl font-bold text-blue-800">
+          Cadastro de usuário
+        </h1>
+        <p className="mt-4 text-gray-700">
+          Por favor, insira suas informações.
+        </p>
+      </div>
+    </main>
+  );
 }
