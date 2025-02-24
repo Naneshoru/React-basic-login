bb1a0f6caa47c544bacc0d2b1da1d6450ffbbbf0
Ricardo Atakiama <ricardo_cssg@hotmail.com>
2025-02-24 08:36:08 -0300

    feat: interface do login

---
+++ src/components/debug.tsx
@@ -0,0 +1,45 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, { useEffect, useState } from 'react'
+
+type Props = {
+  value: any
+  lowOpacity?: boolean
+  children?: any
+}
+function Debug ({ value, lowOpacity, children }: Props): any {
+  const [valueLocal, setValueLocal] = useState<any>()
+  useEffect(() => { setValueLocal(value) }, [value])
+
+  const elem = (document.getElementsByClassName('debug')?.[1] as any)
+  if (elem) {
+    elem.style.left = ''
+    elem.style.right = '30px'
+  }
+
+  const [render, setRender] = useState<any>(false)
+
+  return (
+    <div className='debug custom-scroll'
+      style={{
+        overflow: 'auto',
+        position: 'fixed',
+        left: '30px',
+        top: '100px',
+        maxHeight: '80vh',
+        background: 'white',
+        zIndex: 100,
+        opacity: lowOpacity ? 0.6 : 1
+      }}
+    >
+      <button onClick={(() => { setRender((prev: boolean) => !prev) })}>Update</button>
+      &nbsp; {JSON.stringify(render, null, 2)}
+      <pre>
+        {JSON.stringify(valueLocal, null, 2)}
+        <br></br>
+        {children}
+      </pre>
+    </div>
+  )
+}
+
+export default Debug
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,20 +1,40 @@
+import { Button } from 'components/ui/button';
+import Debug from 'components/debug';
 import { Input } from 'components/ui/input';
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Login() {
+  const [credentials, setCredentials] = useState({
+    email: '',
+    password: ''
+  })
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const name = e.target.name
+    const value = e.target.value
+    setCredentials((prev) => ({ ...prev, [name]: value }))
+  }
+
   return (
     <main>
-      <div className="p-6 bg-blue-100 grow">
-        <h1 className="text-3xl font-bold text-blue-800">
-          Cadastro de usuário
-        </h1>
-        <p className="mt-4 text-gray-700">
-          Por favor, insira suas informações.
-        </p>
+      <Debug value={credentials} />
+      <div className="flex justify-center grow p-6 bg-blue-50">
+        <div className="w-96 bg-blue-100 p-6">
+          <h1 className="text-3xl font-bold text-blue-800">
+            Cadastro de usuário
+          </h1>
+          <p className="mt-4 text-gray-700">
+            Por favor, insira suas informações.
+          </p>
 
-        <Input type="email" placeholder="Email" />
+          <form action="" className='flex flex-col gap-2 p-2'>
+            <Input type="email" name="email" placeholder="email@provedor.com.br" value={credentials.email} onChange={handleChange} className='bg-gray-50' />
 
-        <Input type="password" placeholder='******' />
+            <Input type="password" name="password" placeholder='******' value={credentials.password} onChange={handleChange} className='bg-gray-50' />
+
+            <Button>Cadastrar</Button>
+          </form>
+        </div>
       </div>
     </main>
   );
