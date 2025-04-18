916ac8233a8c31ca2c7f941aebc1de2427c51ebd
Ricardo Atakiama <ricardo_cssg@hotmail.com>
2025-04-18 01:47:03 -0300

    feat: componente de input para a senha, com botão para exibir/esconder

---
+++ src/components/ui/input-password.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { Lock, Unlock } from "lucide-react";
+import { Input } from "./input";
+import 'styles/input.css'
+
+const PasswordInput: React.FC<any> = ({ ...props }: any) => {
+  const [isLocked, setIsLocked] = useState(true);
+
+  const toggleLock = () => {
+    setIsLocked(!isLocked);
+  };
+
+  return (
+      <Input 
+        {...props} 
+        type={isLocked ? 'password' : 'text'} 
+        icon={
+          <span className="lock-icon" onClick={toggleLock}>
+            {isLocked ? <Lock size={20} /> : <Unlock size={20} />}
+          </span>
+        }
+      />
+  );
+};
+
+export default PasswordInput;
